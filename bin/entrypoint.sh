e3079a2c4280f5ba812c087473509ee3b721adf1
Lee Myring <mail@thinkstack.io>
2018-05-25 15:50:54 +0100

    Improve spec; test against real kdbx file; add .rubocop.yml

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-echo "hello world"
+if [ -z "$KEEPASSX_PASSWORD" ]; then
+  echo "No password specified"
+  exit 1;
+fi
+
+if [ -z "$KEEPASSX_ENTRY" ]; then
+  echo "No entry specified"
+  exit 1;
+fi
+
+type kpcli >/dev/null 2>&1 || { echo >&2 "kpcli required but it's not installed.  Aborting."; exit 1; }
+
+echo $KEEPASSX_PASSWORD > /bin/keepassx.pwd
+
+kpcli --kdb /bin/keepassx.kdbx --pwfile /bin/keepassx.pwd --readonly --command "show -f $KEEPASSX_ENTRY" | grep Pass | sed 's/\ Pass\:\ //g' > /bin/keepassx.out
+
+cat /bin/keepassx.out
+
+if [ "$DOCKER_SPEC_KEEPALIVE" = 'true' ]; then tail -f /dev/null; fi
--- spec/kpcli_spec.rb
+++ spec/kpcli_spec.rb
@@ -3,44 +3,57 @@
 require 'spec_helper'
 
 describe 'kpcli Installation' do
-  describe docker_build('Dockerfile', rm: true) do
+  describe docker_build(path: '.', tag: 'thinkstackio/kpcli', rm: true) do
     it { should have_label 'Author' => 'Lee Myring <mail@thinkstack.io>' }
     it { should have_label 'Description' => 'kpcli wrapper instance' }
     it { should have_maintainer 'Lee Myring' }
     it { should have_user 'nobody' }
+    it { should have_entrypoint '/bin/entrypoint.sh' }
 
     its(:arch) { should eq 'amd64' }
     its(:os) { should eq 'linux' }
 
-    describe docker_run(described_image) do
-      # describe file('/src') do
-      #   it { should be_directory }
-      # end
-      #
-      # describe file('/src/requirements.txt') do
-      #   it { should be_file }
-      #   it { should contain 'jmespath==0.9.3' }
-      # end
-      #
-      # %w[bash gcc].each do |package|
-      #   describe "Describe package #{package}" do
-      #     describe package(package) do
-      #       it { should be_installed }
-      #     end
-      #
-      #     it 'has package in the path' do
-      #       expect(command("which #{package}").exit_status).to eq 0
-      #     end
-      #   end
-      # end
-      #
-      # describe command('pidof supervisord') do
-      #   its(:stdout) { should match /1/ }
-      # end
-      #
-      # describe port(5000) do
-      #   it { should be_listening.with('tcp') }
-      # end
+    describe docker_build('spec/', tag: 'thinkstackio/kpcli_test') do
+      docker_env = {
+        'KEEPASSX_PASSWORD' => 'password',
+        'KEEPASSX_ENTRY' => '/Root/Test\ Group/Test',
+        'DOCKER_SPEC_KEEPALIVE' => 'true'
+      }
+      wait = ENV['TRAVIS'] ? 10 : 2
+
+      describe docker_run('thinkstackio/kpcli_test', family: 'debian', env: docker_env, wait: wait) do
+        %w[
+          /usr/bin/kpcli
+          /bin/keepassx.kdbx
+          /bin/keepassx.pwd
+          /bin/keepassx.out
+        ].each do |file|
+          describe file(file) do
+            it { should exist }
+            it { should be_file }
+            it { should be_owned_by 'root' }
+          end
+        end
+
+        %w[kpcli].each do |package|
+          describe "Describe package #{package}" do
+            describe package(package) do
+              it { should be_installed }
+            end
+
+            it 'has package in the path' do
+              expect(command("which #{package}").exit_status).to eq 0
+            end
+          end
+        end
+
+        describe file('/bin/keepassx.out') do
+          it { should exist }
+          it { should be_file }
+          it { should be_owned_by 'root' }
+          its(:content) { should eq("IAmAPasswordSafelySecured\n") }
+        end
+      end
     end
   end
 end
